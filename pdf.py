1f8e2a4816e0b0f58cbd2776ec7e5c0be562794f
Emre Erarslan <18680401+erarslan@users.noreply.github.com>
2023-10-17 14:37:25 +0300

    editing

--- pdf.py
+++ pdf.py
@@ -1,9 +1,20 @@
-# Burası tabula ile değiştirelecek (opsiyonel)
-
 import camelot
 df = camelot.read_pdf("BM1.pdf")[0].df
+
+# DataFrame Editing
+
+df = df.drop(10)
+df[3][0] = "Salı"
 df[4][0] = "Çarşamba"
 df[5][0] = "Perşembe"
+df[1][0] = "Saatler"
+
+for i in range(df.shape[1]):
+    df[i][5] = "Öğle Arası"
+
+df = df.drop(0, axis=1)
+
+# Creating Program
 
 def createProgram (day):
     list = []
@@ -24,7 +35,7 @@ def createProgram (day):
     for i in range(len(list)):
         if list[i] == "Pazartesi":
             list[i] = f"<b>{list[i]}:</b>\n"
-        elif list[i] == "Salı\nÇarşamba\nPerşembe":
+        elif list[i] == "Salı":
             list[i] = "<b>Salı:</b>\n"
         elif list[i] == "Cuma":
             list[i] = "<b>Cuma:</b>\n"
